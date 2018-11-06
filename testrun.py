11d39ba5216ba84f3ef6df29a4c2d6391e54c4d3
torietyler <44810528+torietyler@users.noreply.github.com>
2018-11-06 08:14:45 -0700

    Add files via upload

---
+++ testrun.py
@@ -0,0 +1,8 @@
+import cocos
+from cocos.director import director
+
+class helloWorld (cocos.layer.Layer):
+    def __main__(selfself):
+        super().__init_subclass__()
+        label = cocos.text.label('Hello World',font_name= "Times New Roman", font_size= 45 )
+
